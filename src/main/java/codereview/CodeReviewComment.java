388eac36edf6e18ceead4d0c39a0ff81680eabe9
Mateusz Sosnowski <mateusz.sosnowski@logintegra.com>
2020-01-10 15:41:29 +0100

    initial commit

---
+++ src/main/java/codereview/CodeReviewComment.java
@@ -0,0 +1,8 @@
+
+Dodaj komentarz do tego pliku.
+
+Twój komentarz może być dodany per linia.
+
+Komentarz powinien być dodawany przy linii, której dotyczy dany komentarz.
+
+Jeżeli komentujemy metodę, to komentarz można dać do pierwszej linii metody.
---
+++ src/main/java/codereview/pages/Error404.java
@@ -0,0 +1,30 @@
+package codereview.pages;
+
+public class Error404
+{
+
+}
+
+
+
+
+
+    public String generateRandomPassword(){
+
+    }
+
+    public String generateRandPassword(){
+
+    }
+
+    public String genRanPass(){
+
+    }
+
+    public String grp(){
+
+    }
+
+
+
+    
